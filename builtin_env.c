757b76dea7487d43278dbe97c1dac437393c2dc8
segunlawal <lawalsegun72@gmail.com>
2023-02-22 12:39:36 -0800

    add helper file

--- builtin_env.c
+++ builtin_env.c
@@ -1,22 +1,5 @@
 #include "shell.h"
 /**
- * _strlen - returns length of a string
- * @s: string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int longi = 0;
-
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-	return (longi);
-}
-/**
  * builtin_env - prints environment variables
  * Return: Nothing
  */
