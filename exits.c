af986feab861dd64f7fc122a5e4292ba519e5a5d
segunlawal <lawalsegun72@gmail.com>
2023-02-24 12:30:11 -0800

    betty fix

--- exits.c
+++ exits.c
@@ -8,25 +8,27 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i, j;
-        char *s = dest;
-        i = 0;
-        while (src[i] != '\0' && i < n - 1)
-        {
-                dest[i] = src[i];
-                i++;
-        }
-        if (i < n)
-        {
-                j = i;
-                while (j < n)
-                {
-                        dest[j] = '\0';
-                        j++;
-                }
-        }
-        return (s);
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
+
 /**
  **_strncat - concatenates two strings
  *@dest: the first string
@@ -36,22 +38,24 @@ char *_strncpy(char *dest, char *src, int n)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j;
-        char *s = dest;
-        i = 0;
-        j = 0;
-        while (dest[i] != '\0')
-                i++;
-        while (src[j] != '\0' && j < n)
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-        if (j < n)
-                dest[i] = '\0';
-        return (s);
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
+
 /**
  **_strchr - locates a character in a string
  *@s: the string to be parsed
@@ -60,10 +64,11 @@ char *_strncat(char *dest, char *src, int n)
  */
 char *_strchr(char *s, char c)
 {
-        do {
-                if (*s == c)
-                        return (s);
-        } while (*s++ != '\0');
-        return (NULL);
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
 
