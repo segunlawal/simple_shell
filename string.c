10ed7c151b57edb461b53d04e03081872e2e423c
segunlawal <lawalsegun72@gmail.com>
2023-02-24 14:29:28 -0800

    betty fix

--- string.c
+++ string.c
@@ -7,13 +7,15 @@
  */
 int _strlen(char *s)
 {
-        int i = 0;
-        if (!s)
-                return (0);
-        while (*s++)
-                i++;
-        return (i);
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
 }
+
 /**
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
@@ -23,17 +25,17 @@ int _strlen(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-        while (*s1 && *s2)
-        {
-                if (*s1 != *s2)
-                        return (*s1 - *s2);
-                s1++;
-                s2++;
-        }
-        if (*s1 == *s2)
-                return (0);
-        else
-                return (*s1 < *s2 ? -1 : 1);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
 }
 /**
  * starts_with - checks if needle starts with haystack
@@ -44,10 +46,10 @@ int _strcmp(char *s1, char *s2)
  */
 char *starts_with(const char *haystack, const char *needle)
 {
-        while (*needle)
-                if (*needle++ != *haystack++)
-                        return (NULL);
-        return ((char *)haystack);
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
 }
 /**
  * _strcat - concatenates two strings
@@ -58,12 +60,13 @@ char *starts_with(const char *haystack, const char *needle)
  */
 char *_strcat(char *dest, char *src)
 {
-        char *ret = dest;
-        while (*dest)
-                dest++;
-        while (*src)
-                *dest++ = *src++;
-        *dest = *src;
-        return (ret);
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
 
--- string1.c
+++ string1.c
@@ -8,17 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-        int i = 0;
-        if (dest == src || src == 0)
-                return (dest);
-        while (src[i])
-        {
-                dest[i] = src[i];
-                i++;
-        }
-        dest[i] = 0;
-        return (dest);
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
 }
+
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
@@ -27,19 +29,21 @@ char *_strcpy(char *dest, char *src)
  */
 char *_strdup(const char *str)
 {
-        int length = 0;
-        char *ret;
-        if (str == NULL)
-                return (NULL);
-        while (*str++)
-                length++;
-        ret = malloc(sizeof(char) * (length + 1));
-        if (!ret)
-                return (NULL);
-        for (length++; length--;)
-                ret[length] = *--str;
-        return (ret);
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
 }
+
 /**
  * _puts - prints an input string
  * @str: the string to be printed
@@ -48,15 +52,17 @@ char *_strdup(const char *str)
  */
 void _puts(char *str)
 {
-        int i = 0;
-        if (!str)
-                return;
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i++;
-        }
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -66,15 +72,16 @@ void _puts(char *str)
  */
 int _putchar(char c)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(1, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
