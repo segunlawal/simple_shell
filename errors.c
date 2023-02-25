7b79c05806a43a7327b10368cf7657c1e1cf7354
Nonnyman16 <111024552+Nonnyman16@users.noreply.github.com>
2023-02-25 01:11:37 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -43,7 +43,7 @@ int _eputchar(char c)
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
- * @fd: The filedescriptor to write to
+ * @fd: The file descriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
